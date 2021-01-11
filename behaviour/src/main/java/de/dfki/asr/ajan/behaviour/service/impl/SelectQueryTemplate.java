1ae0ab719679f5594bac9b918e40efb62c5aa47d
unknown <andre.antakli@dfki.de>
2021-01-11 10:07:15 +0100

    extended SelectQueryTemplate to handle array templates

--- behaviour/src/main/java/de/dfki/asr/ajan/behaviour/service/impl/SelectQueryTemplate.java
+++ behaviour/src/main/java/de/dfki/asr/ajan/behaviour/service/impl/SelectQueryTemplate.java
@@ -31,10 +31,13 @@ import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.query.Binding;
 import org.eclipse.rdf4j.query.BindingSet;
 import org.eclipse.rdf4j.repository.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@RDFBean("bt:SelectQueryTemplate")
 @Data
+@RDFBean("bt:SelectQueryTemplate")
 public class SelectQueryTemplate {
+
 	@RDFSubject
 	private String id;
 
@@ -44,20 +47,53 @@ public class SelectQueryTemplate {
 	@RDF("bt:stringTemplate")
 	private String template;
 
+	protected static final Logger LOG = LoggerFactory.getLogger(SelectQueryTemplate.class);
 	private final ValueFactory vf = SimpleValueFactory.getInstance();
+
+	/*
+	$[start]$|$groupName$|
+		$[start]$|$id$|$name$|$[end]$
+		$[start]$|$room$|$[end]$
+	$[end]$
+	*/
 
 	public String getResult(final Repository repo) {
 		List<BindingSet> result = btQuery.getResult(repo);
-		String tmpString = "";
+		String tmpString = template;
+		StringPart part = new StringPart();
+		setTmpPart(template, part);
+		LOG.info(part.getStartContent());
 		Iterator<BindingSet> setItr = result.iterator();
 		while (setItr.hasNext()) {
 			BindingSet set = setItr.next();
 			Iterator<Binding> itr = set.iterator();
 			while (itr.hasNext()) {
 				Binding bdg = itr.next();
-				tmpString = template.replace("$" + bdg.getName() + "$", bdg.getValue().stringValue());
+				tmpString = tmpString.replace("$" + bdg.getName() + "$", bdg.getValue().stringValue());
 			}
 		}
 		return tmpString;
+	}
+
+	private void setTmpPart(final String template, final StringPart part) {
+		int strId = template.indexOf("$[start]$");
+		int endId = template.indexOf("$[end]$");
+		if (template.contains("$[start]$") && strId < endId) {
+			String[] starts = template.split("(\\$\\[start\\]\\$)", 2);
+			part.setStartContent(part.getStartContent() + starts[0]);
+			StringPart child = new StringPart();
+			setTmpPart(starts[1], child);
+			part.getPartChilds().add(child);
+			setTmpPart(child.getRestContent(), part);
+		}
+		if (template.contains("$[end]$") && (strId > endId) || strId == -1) {
+			String[] ends = template.split("(\\$\\[end\\]\\$)", 2);
+			part.setEndContent(part.getEndContent() + ends[0]);
+			if (ends.length > 1) {
+				part.setRestContent(part.getRestContent() + ends[1]);
+			} else {
+				part.setRestContent("");
+			}
+		}
 	}
 }
