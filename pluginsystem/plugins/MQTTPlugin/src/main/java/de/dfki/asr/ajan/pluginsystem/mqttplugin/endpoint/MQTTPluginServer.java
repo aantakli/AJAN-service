5a9db65279aee5fb70a017c5ad25d4d765200e45
bhpo00001 <bhpo00001@stud.uni-saarland.de>
2021-12-16 19:03:01 +0100

    Initial Plugin Structure created for MQTTPlugin

---
+++ pluginsystem/plugins/MQTTPlugin/src/main/java/de/dfki/asr/ajan/pluginsystem/mqttplugin/endpoint/MQTTPluginServer.java
@@ -0,0 +1,18 @@
+package de.dfki.asr.ajan.pluginsystem.mqttplugin.endpoint;
+
+import de.dfki.asr.ajan.pluginsystem.extensionpoints.EndpointExtension;
+import ro.fortsoft.pf4j.Extension;
+
+@Extension
+public class MQTTPluginServer implements EndpointExtension {
+
+    @Override
+    public void initServer(Object agentManager) {
+
+    }
+
+    @Override
+    public void destroyServer() {
+
+    }
+}
