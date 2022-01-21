c346480b6431b2c36b3a520bff66cda9531e53c0
anan02-admin <anan02@dfki.de>
2022-01-21 13:58:28 +0100

    implemented CSV handeling

---
+++ executionservice/src/main/java/de/dfki/asr/ajan/rest/providers/CSVConsumer.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2020 see AJAN-service/AUTHORS.txt (German Research Center for Artificial Intelligence, DFKI).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package de.dfki.asr.ajan.rest.providers;
+
+import com.opencsv.CSVReader;
+import de.dfki.asr.ajan.common.CSVInput;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.Provider;
+import org.springframework.stereotype.Component;
+
+@Provider
+@Consumes("text/csv")
+@SuppressWarnings({"PMD.ExcessiveParameterList", "PMD.AvoidPrefixingMethodParameters"})
+@Component
+public class CSVConsumer implements MessageBodyReader<CSVInput> {
+
+    @Override
+    public boolean isReadable(final Class<?> type, final Type type1, final Annotation[] antns, final MediaType mt) {
+        if (!type.isAssignableFrom(CSVInput.class)) {
+            return false;
+        }
+        return mt.toString().equals("text/csv") ;
+    }
+
+    @Override
+    public CSVInput readFrom(final Class<CSVInput> t, final Type type, final Annotation[] annts, final MediaType mt, final MultivaluedMap<String, String> mm, final InputStream in) throws IOException, WebApplicationException {
+        if (!mt.toString().equals("text/csv")) {
+            String msg = "Can not consume XML of mimetype + " + mt.toString();
+            Response response = Response.status(Response.Status.BAD_REQUEST).type(MediaType.TEXT_PLAIN).entity(msg).build();
+            throw new WebApplicationException(response);
+        }
+        CSVReader reader = new CSVReader(new InputStreamReader(in));
+        CSVInput input = new CSVInput();
+        input.setContent(reader.readAll());
+	reader.close();
+        return input;
+    }
+
+}
