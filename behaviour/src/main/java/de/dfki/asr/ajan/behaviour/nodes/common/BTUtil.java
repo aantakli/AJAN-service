8a3a62e204374da59bf899caf2c78fcc6a52b30e
Andre_Antakli <andre.antakli@dfki.de>
2022-03-21 14:55:40 +0100

    fixed issue with reading out model of a generated ASP - bt

--- behaviour/src/main/java/de/dfki/asr/ajan/behaviour/nodes/common/BTUtil.java
+++ behaviour/src/main/java/de/dfki/asr/ajan/behaviour/nodes/common/BTUtil.java
@@ -119,7 +119,11 @@ public final class BTUtil {
 		List<Resource> list = new ArrayList();
 		for (int i = 0; i < task.getChildCount(); i++) {
 			TreeNode chNode = (TreeNode) task.getChild(i);
-			list.add(chNode.getInstance(btRoot.getInstance()));
+			Resource instance = chNode.getInstance(btRoot.getInstance());
+			if (instance == null) {
+				instance = BTUtil.getInstanceResource(chNode.getUrl(), btRoot.getInstance());
+			}
+			list.add(instance);
 			chNode.getModel(model, btRoot, mode);
 		}
 		RDFCollections.asRDF(list, head, model);
