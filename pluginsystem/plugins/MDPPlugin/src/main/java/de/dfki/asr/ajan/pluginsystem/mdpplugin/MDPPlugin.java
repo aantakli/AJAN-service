a916711d7e41417d68cada52f2b2b7a7b3ab9376
Bhuvaneshwaran I <bhpo00001@stud.uni-saarland.de>
2022-10-29 23:22:21 +0200

    Created MDPPlugin

---
+++ pluginsystem/plugins/MDPPlugin/src/main/java/de/dfki/asr/ajan/pluginsystem/mdpplugin/MDPPlugin.java
@@ -0,0 +1,29 @@
+package de.dfki.asr.ajan.pluginsystem.mdpplugin;
+
+import org.pf4j.Plugin;
+import org.pf4j.PluginWrapper;
+import org.pf4j.RuntimeMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MDPPlugin extends Plugin {
+    private static final Logger LOG = LoggerFactory.getLogger(MDPPlugin.class);
+
+    public MDPPlugin(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public void start(){
+        System.out.println("MDPPlugin.start()");
+        if(RuntimeMode.DEVELOPMENT.equals(wrapper.getRuntimeMode())){
+            LOG.debug("MDPPlugin");
+        }
+    }
+
+    @Override
+    public void stop(){
+        System.out.println("MDPPlugin.stop()");
+    }
+
+}
