e60bace09273dac203a0277f45368e4f6db47148
Bhuvaneshwaran I <bhpo00001@stud.uni-saarland.de>
2022-10-30 20:56:47 +0100

    Added code to initialize server for testing RDDL server start

--- pluginsystem/plugins/MDPPlugin/src/main/java/de/dfki/asr/ajan/pluginsystem/mdpplugin/MDPPlugin.java
+++ pluginsystem/plugins/MDPPlugin/src/main/java/de/dfki/asr/ajan/pluginsystem/mdpplugin/MDPPlugin.java
@@ -5,6 +5,8 @@ import org.pf4j.PluginWrapper;
 import org.pf4j.RuntimeMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import rddl.competition.Client;
+import rddl.competition.Server;
 
 public class MDPPlugin extends Plugin {
     private static final Logger LOG = LoggerFactory.getLogger(MDPPlugin.class);
@@ -18,6 +20,8 @@ public class MDPPlugin extends Plugin {
         System.out.println("MDPPlugin.start()");
         if(RuntimeMode.DEVELOPMENT.equals(wrapper.getRuntimeMode())){
             LOG.debug("MDPPlugin");
+            String[] arg = new String[] {"D:\\Bhuvan\\Projects\\PRobPlan\\projects\\test\\files"};
+            Server.main(arg);
         }
     }
 
