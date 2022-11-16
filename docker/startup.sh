56e48d854c3e888d4774684c0fb33bbcbb40c151
Yannic Hock <mail@yannic-hock.de>
2022-11-16 12:21:45 +0100

    Removed trigger to create.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,7 +4,6 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
-    /bin/sh /app/create.sh &
     /usr/bin/supervisord -c /app/supervisord.conf
 else
     /usr/bin/supervisord -c /app/supervisord.conf
