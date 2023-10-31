8961e3dde2a5b30ad7d734837988a9d565e8c4ec
Bhuvaneshwaran Ilanthirayan <buvnswrn@gmail.com>
2023-10-31 17:14:43 +0100

    Changed the URI from Ajan to DFKI

--- pluginsystem/plugins/MDPPlugin/src/main/java/de/dfki/asr/ajan/pluginsystem/mdpplugin/vocabularies/POMDPVocabulary.java
+++ pluginsystem/plugins/MDPPlugin/src/main/java/de/dfki/asr/ajan/pluginsystem/mdpplugin/vocabularies/POMDPVocabulary.java
@@ -15,27 +15,33 @@ import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 public class POMDPVocabulary {
     private static final ValueFactory FACTORY = SimpleValueFactory.getInstance();
 
-    public final static IRI POMDP = FACTORY.createIRI("http://www.ajan.de/behavior/pomdp-ns#POMDP");
-    public final static IRI STATE = FACTORY.createIRI("http://www.ajan.de/behavior/pomdp-ns#State");
-    public final static IRI ACTION = FACTORY.createIRI("http://www.ajan.de/behavior/pomdp-ns#Action");
-    public final static IRI OBSERVATION = FACTORY.createIRI("http://www.ajan.de/behavior/pomdp-ns#Observation");
+    public final static IRI POMDP = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#POMDP");
+
+    public final static IRI HAS_ID = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#id");
+    public final static IRI NAME = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#name");
+    public final static IRI TYPE= FACTORY.createIRI("http://www.dfki.de/pomdp-ns#type");
+    public final static IRI HAS_INITIAL_BELIEF = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#initialBelief");
+    public final static IRI STATE = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#State");
+    public final static IRI BELIEF = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#Belief");
+    public final static IRI ACTION = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#Action");
+    public final static IRI OBSERVATION = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#Observation");
 
     // state1 --hasReward--> <some Number>
-    public final static IRI HAS_REWARD = FACTORY.createIRI("http://www.ajan.de/behavior/pomdp-ns#hasReward");
+    public final static IRI HAS_REWARD = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#hasReward");
 
     //pomdp --solver--> "DESPOT"
-    public final static IRI SOLVER_NAME = FACTORY.createIRI("http://www.ajan.de/behavior/pomdp-ns#solver");
+    public final static IRI SOLVER_NAME = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#solver");
 
     // state1 --changes to--> emptyNode
-    public final static IRI CHANGES_TO = FACTORY.createIRI("http://www.ajan.de/behavior/pomdp-ns#changesTo");
+    public final static IRI CHANGES_TO = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#changesTo");
 
     // state/action/observation --label--> LEFT/RIGHT/LISTEN
-    public final static IRI HAS_LABEL = FACTORY.createIRI("http://www.ajan.de/behavior/pomdp-ns#label");
+    public final static IRI HAS_LABEL = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#label");
 
     // :emptyNode --with-action--> state 2
-    public final static IRI WITH_ACTION = FACTORY.createIRI("http://www.ajan.de/behavior/pomdp-ns#with");
+    public final static IRI WITH_ACTION = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#with");
 
     // :emptyNode --with-probability--> 0.5
-    public final static IRI WITH_PROBABILITY = FACTORY.createIRI("http://www.ajan.de/behavior/pomdp-ns#probability");
+    public final static IRI WITH_PROBABILITY = FACTORY.createIRI("http://www.dfki.de/pomdp-ns#probability");
 
 }
