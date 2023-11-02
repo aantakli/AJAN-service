5351158d6540087e200e6ca8fe7de47663dbf3f4
Bhuvaneshwaran Ilanthirayan <buvnswrn@gmail.com>
2023-11-02 13:12:27 +0100

    Added graph data logic

--- pluginsystem/plugins/MDPPlugin/src/main/java/de/dfki/asr/ajan/pluginsystem/mdpplugin/extensions/models/ObservationModel.java
+++ pluginsystem/plugins/MDPPlugin/src/main/java/de/dfki/asr/ajan/pluginsystem/mdpplugin/extensions/models/ObservationModel.java
@@ -5,9 +5,11 @@ import de.dfki.asr.ajan.behaviour.nodes.common.AbstractTDBLeafTask;
 import de.dfki.asr.ajan.behaviour.nodes.common.BTUtil;
 import de.dfki.asr.ajan.behaviour.nodes.common.EvaluationResult;
 import de.dfki.asr.ajan.behaviour.nodes.common.NodeStatus;
+import de.dfki.asr.ajan.behaviour.nodes.query.BehaviorConstructQuery;
 import de.dfki.asr.ajan.behaviour.nodes.query.BehaviorSelectQuery;
 import de.dfki.asr.ajan.pluginsystem.extensionpoints.NodeExtension;
 import de.dfki.asr.ajan.pluginsystem.mdpplugin.extensions.datamodels.Attribute;
+import de.dfki.asr.ajan.pluginsystem.mdpplugin.utils.HTTPHelper;
 import lombok.Getter;
 import lombok.Setter;
 import org.cyberborean.rdfbeans.annotations.RDF;
@@ -15,9 +17,17 @@ import org.cyberborean.rdfbeans.annotations.RDFBean;
 import org.cyberborean.rdfbeans.annotations.RDFSubject;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFWriter;
+import org.eclipse.rdf4j.rio.Rio;
+import org.json.JSONObject;
 import org.pf4j.Extension;
 import org.springframework.stereotype.Component;
+import org.eclipse.rdf4j.repository.Repository;
 
+import java.io.StringWriter;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @Getter
@@ -43,6 +53,10 @@ public class ObservationModel extends AbstractTDBLeafTask implements NodeExtensi
     @Getter @Setter
     private List<Attribute> attributes;
 
+    @RDF("bt-mdp:observation-data")
+    @Getter @Setter
+    private BehaviorConstructQuery data;
+
     @RDF("bt-mdp:observation-probability")
     @Getter @Setter
     private BehaviorSelectQuery probability;
@@ -58,7 +72,42 @@ public class ObservationModel extends AbstractTDBLeafTask implements NodeExtensi
 
     @Override
     public NodeStatus executeLeaf() {
-        return new NodeStatus(Status.SUCCEEDED, this.getObject().getLogger(), this.getClass(), this +" SUCCEEDED");
+        try {
+            Repository repository = BTUtil.getInitializedRepository(getObject(),data.getOriginBase());
+            Model model = data.getResult(repository);
+            String data = getString(model);
+            JSONObject params = getParams(data);
+
+            int responseCode = HTTPHelper.sendPostRequest("http://127.0.0.1:8000/AJAN/pomdp/observation_model/create/init-model", params, this.getObject().getLogger(), this.getClass());
+            if(responseCode >= 300){
+                return new NodeStatus(Status.FAILED, this.getObject().getLogger(), this.getClass(), this+"FAILED");
+            }
+            return new NodeStatus(Status.SUCCEEDED, this.getObject().getLogger(), this.getClass(), this +" SUCCEEDED");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private JSONObject getParams(String data) {
+        JSONObject params = new JSONObject();
+        params.put("data", data);
+        params.put("probability_query", probability.toString());
+        params.put("sample_query", sample.toString());
+        params.put("argmax_query", argmax.toString());
+        return params;
+    }
+
+    private static String getString(Model model) {
+        StringWriter writer = new StringWriter();
+        RDFWriter rdfWriter = Rio.createWriter(RDFFormat.TURTLE,writer);
+
+        rdfWriter.startRDF();
+        for (Statement statement: model) {
+            rdfWriter.handleStatement(statement);
+        }
+        rdfWriter.endRDF();
+
+        return writer.toString();
     }
 
     @Override
