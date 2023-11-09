1fd23e776594d9b280e41fd43edc5f4d9ec9655a
Bhuvaneshwaran Ilanthirayan <buvnswrn@gmail.com>
2023-11-09 12:06:16 +0100

    Added proper action definition.

--- pluginsystem/plugins/MDPPlugin/src/main/java/de/dfki/asr/ajan/pluginsystem/mdpplugin/extensions/domain/Action.java
+++ pluginsystem/plugins/MDPPlugin/src/main/java/de/dfki/asr/ajan/pluginsystem/mdpplugin/extensions/domain/Action.java
@@ -7,6 +7,7 @@ import de.dfki.asr.ajan.behaviour.nodes.common.EvaluationResult;
 import de.dfki.asr.ajan.behaviour.nodes.common.NodeStatus;
 import de.dfki.asr.ajan.pluginsystem.extensionpoints.NodeExtension;
 import de.dfki.asr.ajan.pluginsystem.mdpplugin.extensions.datamodels.Attribute;
+import de.dfki.asr.ajan.pluginsystem.mdpplugin.utils.HTTPHelper;
 import lombok.Getter;
 import lombok.Setter;
 import org.cyberborean.rdfbeans.annotations.RDF;
@@ -14,6 +15,7 @@ import org.cyberborean.rdfbeans.annotations.RDFBean;
 import org.cyberborean.rdfbeans.annotations.RDFSubject;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Resource;
+import org.json.JSONObject;
 import org.pf4j.Extension;
 import org.springframework.stereotype.Component;
 
@@ -36,8 +38,19 @@ public class Action extends AbstractTDBLeafTask implements NodeExtension {
     @Setter
     private String actionName;
 
+    @RDF("bt-mdp:pomdpId")
+    @Setter
+    private int pomdpId;
+
     @Override
     public NodeStatus executeLeaf() {
+        JSONObject params = new JSONObject();
+        params.put("action_name", actionName);
+        params.put("pomdp_id", pomdpId);
+        int responseCode = HTTPHelper.sendPostRequest("http://127.0.0.1:8000/AJAN/pomdp/action/create", params, this.getObject().getLogger(),this.getClass());
+        if (responseCode >= 300 ) {
+            return new NodeStatus(Status.FAILED, this.getObject().getLogger(), this.getClass(), toString()+" FAILED");
+        }
         return new NodeStatus(Status.SUCCEEDED, this.getObject().getLogger(), this.getClass(), toString()+" SUCCEEDED");
     }
 
