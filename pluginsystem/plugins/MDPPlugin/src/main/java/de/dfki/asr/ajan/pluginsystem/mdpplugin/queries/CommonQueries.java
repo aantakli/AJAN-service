71cbe9224f9f863e6e5bff7bf7b667e8eabeef82
Bhuvaneshwaran Ilanthirayan <buvnswrn@gmail.com>
2023-11-23 11:05:08 +0100

    Added common queries to fetch current_observation

--- pluginsystem/plugins/MDPPlugin/src/main/java/de/dfki/asr/ajan/pluginsystem/mdpplugin/queries/CommonQueries.java
+++ pluginsystem/plugins/MDPPlugin/src/main/java/de/dfki/asr/ajan/pluginsystem/mdpplugin/queries/CommonQueries.java
@@ -2,17 +2,19 @@ package de.dfki.asr.ajan.pluginsystem.mdpplugin.queries;
 
 import de.dfki.asr.ajan.behaviour.AgentTaskInformation;
 import de.dfki.asr.ajan.behaviour.nodes.common.BTUtil;
+import de.dfki.asr.ajan.behaviour.nodes.query.BehaviorConstructQuery;
 import de.dfki.asr.ajan.common.AJANVocabulary;
-import de.dfki.asr.ajan.common.SPARQLUtil;
-import de.dfki.asr.ajan.pluginsystem.mdpplugin.utils.POMDPUtil;
-import de.dfki.asr.ajan.pluginsystem.mdpplugin.vocabularies.POMDPVocabulary;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.query.BindingSet;
 import org.eclipse.rdf4j.query.TupleQuery;
 import org.eclipse.rdf4j.query.TupleQueryResult;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -29,6 +31,17 @@ public class CommonQueries {
             "    pomdp-ns:POMDP pomdp-ns:current ?currentPOMDP .\n" +
             "} ";
 
+    public static final String CONSTRUCT_CURRENT_OBSERVATION = "PREFIX pomdp-ns:<http://www.dfki.de/pomdp-ns#>\n" +
+            "PREFIX pomdp-ns1:<http://www.dfki.de/pomdp-ns/>\n" +
+            "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+            "\n" +
+            "CONSTRUCT {?s ?p ?o}\n" +
+            "WHERE{\n" +
+            "    pomdp-ns:CurrentObservation rdf:value ?key .\n" +
+            "    ?key (pomdp-ns:|!pomdp-ns:)* ?o .\n" +
+            "    ?s ?p ?o .\n" +
+            "}";
+
     public static int getCurrentPOMDPId(AgentTaskInformation info) throws URISyntaxException {
         URI originBase = new URI(AJANVocabulary.EXECUTION_KNOWLEDGE.toString());
         Repository repo = BTUtil.getInitializedRepository(info, originBase);
@@ -42,6 +55,22 @@ public class CommonQueries {
             String idString = bindings.get(0).getValue("currentPOMDP").stringValue().replace(pomdp_ns.stringValue(),"");
             return Integer.parseInt(idString);
         }
+    }
+
+    public static String getConstructResult(AgentTaskInformation info, String constructQuery) throws URISyntaxException {
+        URI originBase = new URI(AJANVocabulary.EXECUTION_KNOWLEDGE.toString());
+        Repository repo = BTUtil.getInitializedRepository(info, originBase);
+        BehaviorConstructQuery queryAll = new BehaviorConstructQuery();
+        queryAll.setSparql(constructQuery);
+        Model model = queryAll.getResult(repo);
+        OutputStream out = new ByteArrayOutputStream();
+        Rio.write(model, out, RDFFormat.TURTLE);
+        return out.toString();
+//        try( RepositoryConnection connection = repo.getConnection()){
+//            TupleQuery tupleQuery = connection.prepareTupleQuery(constructQuery);
+//            TupleQueryResult result = tupleQuery.evaluate();
+//            return result.toString();
+//        }
     }
 }
 
