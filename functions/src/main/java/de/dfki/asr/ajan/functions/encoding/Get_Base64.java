87c52562f8170b4d363cfb592c69143e1a051e3c
André Antakli <anan02@dfki.de>
2025-01-29 14:22:07 +0100

    created base64 SPARQL function

---
+++ functions/src/main/java/de/dfki/asr/ajan/functions/encoding/Get_Base64.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2022 Andre Antakli (German Research Center for Artificial Intelligence, DFKI).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package de.dfki.asr.ajan.functions.encoding;
+
+import java.util.Base64;
+import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.query.algebra.evaluation.ValueExprEvaluationException;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.Function;
+
+
+/**
+ *
+ * @author Andre Antakli (German Research Center for Artificial Intelligence,
+ * DFKI)
+ */
+public class Get_Base64 implements Function {
+	public static final String NAMESPACE = "http://www.ajan.de/ajan/functions/encoding-ns#";
+
+	@Override
+	public String getURI() {
+		return NAMESPACE + "base64";
+	}
+
+	@Override
+	public Value evaluate(final ValueFactory valueFactory, final Value... args) throws ValueExprEvaluationException {
+		if (args.length != 1) {
+			throw new ValueExprEvaluationException(
+					"base64 function requires" + "exactly one argument, got "
+							+ args.length);
+		}
+		
+		Value arg = args[0];
+		
+		if (!(arg instanceof Literal)) {
+			throw new ValueExprEvaluationException(
+					"invalid argument (literal expected): " + arg);
+		}
+
+		String value = ((Literal) arg).getLabel();
+
+		return valueFactory.createLiteral(Base64.getEncoder().encodeToString(value.getBytes()));
+	}
+}
