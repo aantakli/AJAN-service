d353ed1548d0f197b31194a8e1d587721df905ec
Yannic Hock <yaho00002@htwsaar.de>
2026-06-01 22:36:08 +0200

    Add Docker deployment config and enhance Java execution flags
    - Introduced `.run/ajan-editor.run.xml` for Docker deployment configuration.
    - Updated Java startup scripts to include `--add-opens` flag for compatibility.
    - Adjusted plugin paths in `startAJAN` scripts for consistency.

--- .github/startAJAN.bat
+++ .github/startAJAN.bat
@@ -20,6 +20,6 @@ for /f "usebackq tokens=1,2 delims==" %%a in (%ENV_FILE%) do (
     )
 )
 
-java -Dtriplestore.initialData.agentFolderPath=use-case/agents -Dtriplestore.initialData.domainFolderPath=use-case/domains -Dtriplestore.initialData.serviceFolderPath=use-case/services -Dtriplestore.initialData.behaviorsFolderPath=use-case/behaviors -Dtriplestore.initialData.nodeDefinitionsFolderPath=use-case/editor/nodeDefinitions -Dtriplestore.initialData.editorDataFolderPath=use-case/editor/editorData -Dpf4j.mode=development -Dserver.port=8080 -DloadTTLFiles=true -Dpf4j.pluginsDir=plugins -Dtriplestore.url=http://localhost:8090/rdf4j -jar executionservice-0.1.jar
+java --add-opens java.base/java.lang=ALL-UNNAMED -Dtriplestore.initialData.agentFolderPath=use-case/agents -Dtriplestore.initialData.domainFolderPath=use-case/domains -Dtriplestore.initialData.serviceFolderPath=use-case/services -Dtriplestore.initialData.behaviorsFolderPath=use-case/behaviors -Dtriplestore.initialData.nodeDefinitionsFolderPath=use-case/editor/nodeDefinitions -Dtriplestore.initialData.editorDataFolderPath=use-case/editor/editorData -Dpf4j.mode=development -Dserver.port=8080 -DloadTTLFiles=true -Dpf4j.pluginsDir=pluginsystem/plugins -Dtriplestore.url=http://localhost:8090/rdf4j -jar executionservice-0.1.jar
 endlocal
 pause
